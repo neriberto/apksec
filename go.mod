0f8fe70f93532b361d09f27b7dfe132cad72f427
Neriberto C.Prado <neriberto@gmail.com>
2023-07-21 10:56:22 -0300

    Use pflag to parse command line

--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/neriberto/apksec
 
 go 1.18
 
+require github.com/avast/apkparser v0.0.0-20221012080151-bfc57d4d0502
+
 require (
-    github.com/avast/apkparser v0.0.0-20221012080151-bfc57d4d0502
-    github.com/klauspost/compress v1.11.8 // indirect
+	github.com/klauspost/compress v1.11.8 // indirect
+	github.com/ogier/pflag v0.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,14 +3,36 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"github.com/avast/apkparser"
 	"os"
+
+	"github.com/avast/apkparser"
+	flag "github.com/ogier/pflag"
 )
 
+// flags
+var (
+	path string
+)
+
+func init() {
+	flag.StringVarP(&path, "filepath", "p", "", "File path for an apk")
+}
+
 func main() {
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Printf("Searching user(s): %s\n", path)
+
 	enc := xml.NewEncoder(os.Stdout)
 	enc.Indent("", "\t")
-	zipErr, resErr, manErr := apkparser.ParseApk(os.Args[1], enc)
+	zipErr, resErr, manErr := apkparser.ParseApk(path, enc)
 	if zipErr != nil {
 		fmt.Fprintf(os.Stderr, "Failed to open the APK: %s", zipErr.Error())
 		os.Exit(1)
